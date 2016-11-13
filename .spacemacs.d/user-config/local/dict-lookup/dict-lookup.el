daead9a85c8da3195a33545486d1a974037b2ef8
Vasiliy Kevroletin <kevroletin@gmail.com>
2016-11-13 16:06:45 +1000

    Implement dict-lookup-show-spelling
    Change lookup prefix key d -> s

--- .spacemacs.d/user-config/local/dict-lookup/dict-lookup.el
+++ .spacemacs.d/user-config/local/dict-lookup/dict-lookup.el
@@ -23,29 +23,36 @@
                (with-stress (s-replace "<span class=\"dbox-bold\">" "`" str))
                (wo-html (replace-regexp-in-string "</?.*?>" "" with-stress))
                (wo-spaces (-map #'s-trim (s-lines wo-html))))
-          (-remove #'s-blank? wo-spaces))))))
+          (-first-item (-remove #'s-blank? wo-spaces)))))))
 
 (defun dict-lookup--find-spelling-in-current-buffer ()
   (dict-lookup--find-spelling-in-buffer (current-buffer)))
 
-(defun dict-lookup-insert-spelling-at-point ()
-  (interactive)
-
-  (let ((word (thing-at-point 'symbol))
-        (original-pos (point)))
-
+(defun dict-lookup--spelling-at-point-common (callback)
+  (-when-let (word (thing-at-point 'symbol))
     (deferred:$
       (request-deferred
        (dict-lookup--url-for-word word)
        :parser 'dict-lookup--find-spelling-in-current-buffer)
       (deferred:nextc it #'request-response-data)
-      (deferred:nextc it
-        (lambda (x)
-          (save-excursion
-            (goto-char original-pos)
-            (forward-symbol 1)
-            (insert " ")
-            (insert (-first-item x))))))))
+      (deferred:nextc it (lambda (x) (funcall callback word x))))))
+
+(defun dict-lookup-insert-spelling-at-point ()
+  (interactive)
+
+  (let ((original-pos (point)))
+    (dict-lookup--spelling-at-point-common
+     (lambda (word spellind)
+       (save-excursion
+         (goto-char original-pos)
+         (forward-symbol 1)
+         (insert " ")
+         (insert spelling))))))
+
+(defun dict-lookup-show-spelling-at-point ()
+  (interactive)
+  (dict-lookup--spelling-at-point-common
+   (lambda (w s) (message"%s %s" w s))))
 
 (defun dict-lookup-browser-at-point ()
   (interactive)
